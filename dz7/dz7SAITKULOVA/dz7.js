69d052cbd2ae43a7810a50450536cf2b772c5766
keldibekovaa <115090119+keldibekovaa@users.noreply.github.com>
2022-10-25 23:20:34 +0600

    dz7

--- dz7/dz7SAITKULOVA/dz7.js
+++ dz7/dz7SAITKULOVA/dz7.js
@@ -16,9 +16,41 @@ class Worker {
     getFullname (){
         return console.log (${this.name} ${this.surname})
     }
-
-}
 //задание 2
-const Aigerim =new Worker('aigerim','Keldibekova','250','30')
-Aigerim.getSalary()
-Aigerim.getFullname()
+    
+}
+class WorkerTwo {
+    #name;
+    #surname;
+    #rate;
+    #days;
+    constructor(name, surname, rate, days) {
+        this.#name = name;
+        this.#surname = surname;
+        this.#rate = rate;
+        this.#days = days;
+    }
+    getName() {
+        return this.#name;
+    }
+    getSurname() {
+        return this.#surname;
+    }
+    getRate() {
+        return this.#rate;
+    }
+    getDays() {
+        return this.#days;
+    }
+    getSalary(rate, days) {
+        let salary = this.#rate * this.#days;
+        return salary;
+    }
+}
+
+workerTwo = new WorkerTwo('AIGERIM ', 'KELDIBEKOVA ', '250', '31');
+console.log(workerTwo.getName());
+console.log(workerTwo.getSurname());
+console.log(workerTwo.getRate());
+console.log(workerTwo.getDays());
+console.log(workerTwo.getSalary());
